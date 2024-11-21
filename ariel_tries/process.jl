ad9557023b9ae8878cb929be2294a01fd82ace95
Ariel Engelman <ariel.engelman1310@gmail.com>
2024-11-21 17:40:27 +0200

    Changed process.jl

--- ariel_tries/process.jl
+++ ariel_tries/process.jl
@@ -3,6 +3,22 @@ using Gurobi
 using HiGHS
 using Images
 using MAT
+using JuMP
+using Printf
+
+# Utils Functions
+function print_summary(d::Dict)
+    # Helper function to print out output
+    obj_val = JuMP.objective_value(d[:Model])
+    solve_time = JuMP.solve_time(d[:Model])
+    println("Objective Value: $(@sprintf("%.6f", obj_val)), Solve Time: $(@sprintf("%.2f", solve_time))")
+end
+
+function view_diff(diff::Array{<:Real, 2})
+    n = 1001
+    colormap("RdBu", n)[ceil.(Int, (diff .+ 1) ./ 2 .* n)]
+end
+
 
 # Including functions
 include("utils/create_sequential_model.jl")
@@ -12,4 +28,24 @@ println("The current dir is: ", pwd())
 path_to_network = "ariel_tries/networks/mnist2.mat" # Path to network
 model = create_sequential_model(path_to_network, "model.n1")
 
+# Choosing the input to find adverserial attack against
+sample_image = MIPVerify.get_image(mnist.test.images, 1)
 
+d = MIPVerify.find_adversarial_example(
+    model,
+    sample_image,
+    9,
+    Gurobi.Optimizer,
+    Dict("output_flag" => false),
+    pp = MIPVerify.LInfNormBoundedPerturbationFamily(0.105),
+    norm_order = Inf,
+    tightening_algorithm = lp,
+)
+print_summary(d)
+
+diff = value.(d[:Perturbation])
+view_diff(diff[1, :, :, 1])
+
+perturbed_input = JuMP.value.(d[:PerturbedInput])
+colorview(Gray, perturbed_input[1, :, :, 1])
+#colorview(Gray, sample_image[1, :, :, 1])

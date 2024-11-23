ce467b93cabbb80bdde0323454afcb365c39c293
NoNameLand <ariel.engelman1310@gmail.com>
2024-11-23 11:02:46 +0200

    New network structure

--- ariel_tries/utils/generate_networks.py
+++ ariel_tries/utils/generate_networks.py
@@ -3,7 +3,7 @@ from nn_utils import train_model, save_model_layers, adjust_model_weights
 nn_path = "ariel_tries/networks.mat"
 dataset_path = "deps/datasets/mnist/mnist_data.mat" #TODO: Add path to mnist dataset
 
-layer_definitions = [
+"""layer_definitions = [
     {
         'type': 'conv',
         'in_channels': 1,         # MNIST images have 1 channel (grayscale)
@@ -17,7 +17,41 @@ layer_definitions = [
         'in_features': 6 * 24 * 24,  # Output from conv layer flattened
         'out_features': 10           # 10 classes for MNIST digits
     }
+]""" # Last layer
+
+layer_definitions = [
+    # First Convolutional Layer
+    {
+        'type': 'conv',
+        'in_channels': 1,         # MNIST images have 1 channel (grayscale)
+        'out_channels': 16,       # Increase the number of filters
+        'kernel_size': 5,         # Kernel size of 5x5
+        'stride': 1,
+        'padding': 2              # Add padding to preserve spatial dimensions
+    },
+    # Second Convolutional Layer
+    {
+        'type': 'conv',
+        'in_channels': 16,
+        'out_channels': 32,
+        'kernel_size': 5,
+        'stride': 1,
+        'padding': 2
+    },
+    # Fully Connected Layer
+    {
+        'type': 'fc',
+        'in_features': 32 * 7 * 7,  # Output from conv layers after 2x2 pooling
+        'out_features': 128         # Dense layer with 128 units
+    },
+    # Final Classification Layer
+    {
+        'type': 'fc',
+        'in_features': 128,
+        'out_features': 10          # 10 classes for MNIST digits
+    }
 ]
+
 
 # Save the model to a .mat file
 save_model_layers(layer_definitions, 'ariel_tries/networks/small_mnist_model.mat')

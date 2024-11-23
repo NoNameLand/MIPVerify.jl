059987d6da3c28bec99e99266680038029fdc039
Ariel Engelman <ariel.engelman1310@gmail.com>
2024-11-23 11:01:51 +0200

    Better DNN Try

--- ariel_tries/process.jl
+++ ariel_tries/process.jl
@@ -23,6 +23,9 @@ end
 # Including functions
 include("utils/create_sequential_model.jl")
 
+# Setting log outputs 
+MIPVerify.set_log_level("debug")
+
 # Loading mnist 
 mnist = MIPVerify.read_datasets("MNIST")
 mnist.train
@@ -31,9 +34,20 @@ mnist.train
 println("The current dir is: ", pwd())
 path_to_network = "ariel_tries/networks/adjusted_small_mnist_model.mat" # Path to network
 model = create_sequential_model(path_to_network, "model.n1")
+println(model)
 
+image_num = 4 # The sample number
 # Choosing the input to find adverserial attack against
-sample_image = MIPVerify.get_image(mnist.test.images, 4)
+sample_image = MIPVerify.get_image(mnist.test.images, image_num)
+
+# Get class of network
+model_output = model(sample_image)
+predicted_class = argmax(model_output)
+println("Predicted Class: ", predicted_class)
+
+if MIPVerify.get_label(mnist.test.labels, image_num) != predicted_class
+    println("The model classified the wanted sample wrong!")
+end
 
 d = MIPVerify.find_adversarial_example(
     model,

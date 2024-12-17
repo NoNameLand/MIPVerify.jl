f5fe16af7e978ce8eaa20cc9be54eaaec56ff258
NoNameLand <ariel.engelman1310@gmail.com>
2024-12-17 12:40:12 +0200

    Adding Function Run

---
+++ ariel_tries/main.jl
@@ -0,0 +1,37 @@
+using ArgParse
+
+# Adding the function of the main
+include("process_bounds.jl")
+
+
+function main()
+    # Define the argument parser
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+        "--network_type"
+            help = "The type of the network to test"
+            arg_type = String
+            required = true
+        "--output", "-o"
+            help = "Path to the output file"
+            arg_type = String
+            default = "output.txt"
+    end
+
+    # Parse the arguments
+    parsed_args = parse_args(ARGS, s)
+
+    # Use the parsed arguments
+    println("Input file: ", parsed_args["network_type"])
+    println("Output file: ", parsed_args["output"])
+
+    println("Processing inputs and writing to output file...")
+
+    # Calling the function
+    process_bounds()
+
+
+end
+
+# Run the script if executed directly
+main()
---
+++ ariel_tries/utils/remote_control/remote_control_functions.py
@@ -0,0 +1,18 @@
+# Imports
+import time
+import subprocess
+
+def run_batch_verification(command, success_string):
+    timeout = 3600 # 1 hour
+    start_time = time.time()
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, timeout=timeout) # seconds
+    except Exception:
+        # timeout has reached
+        return False, timeout / 60
+    runtime = (time.time() - start_time) / 60
+    status = True if success_string in result.stdout else False
+    return status, runtime
+
+
+
